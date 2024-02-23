7094102e4e4546d84ce4bfd378b4323ce10ce065
Jack Crawford <primes_unit.05@icloud.com>
2024-02-23 21:51:42 +1300

    scoring game correctly with spares

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-// Score 64 (simple game):
+// // Score 64 (simple game):
 // const frames = [
 //   [2, 0], [4, 2], [6, 0], [2, 4], [1, 5], [7, 0], [5, 2], [7, 0], [2, 6], [8, 1]
 // ]
@@ -7,9 +7,9 @@
 //   [6, 1], [4, 0], [6, 4], [2, 7], [3, 5], [5, 0], [5, 5], [0, 0], [1, 6], [7, 2]
 // ]
 // Score 104 (with spares and strikes):
-// const frames = [
-//   [6, 4], [8, 0], [10, 0], [2, 7], [5, 5], [4, 0], [10, 0], [2, 1], [2, 6], [4, 4]
-// ]
+const frames = [
+  [6, 4], [8, 0], [10, 0], [2, 7], [5, 5], [4, 0], [10, 0], [2, 1], [2, 6], [4, 4]
+]
 //
 // Score 119 (with spares, strikes and a double strike):
 // const frames = [
@@ -35,47 +35,29 @@
 // singleFrame = a + b
 // totalFrames =  (a + B) + etc
 
-
-
-// function addSingleFrame(a, b){
-//   return a + b
-// }
-
-// let newScoreArray = [ ]
-
-
-// function addAllFrames(frames){
-//   frames.forEach(frame => {
-//     let newScore = addSingleFrame(frame[0], frame[1])
-//     newScoreArray.push(newScore)
+// function scoreSimpleGame(frames){
+//   let result = 0
+//   frames.forEach((frame) => {
+//     result += frame[0] + frame[1]
 //   })
-//   return newScoreArray
+//   return result
 // }
-//returns an array of numbers (sum of first and second bowl in each position)
-// console.log(addAllFrames(frames))
-
-function scoreSimpleGame(frames){
-  let result = 0
-  frames.forEach((frame) => {
-    result += frame[0] + frame[1]
-  })
-  return result
-}
 // console.log(scoreSimpleGame(frames))
-
-
 
 function scoreGameWithSpares(frames){
   let score = 0
   frames.forEach((frame, index) => {
-    if(frame[0] + frame[1] === 10){
-      console.log(frame)
-    }
-  })
-  return score
-}
+    let frameScore = frame[0] + frame[1];
+    score += frameScore
+    if(frameScore === 10 && index < frames.length -1){
+      score += frames[index + 1][0]
+      } 
+    })
+    return score
+  }
 
 console.log(scoreGameWithSpares(frames))
+
 
 
 // if (frames[0][0] + frames[0][1] === 10){
